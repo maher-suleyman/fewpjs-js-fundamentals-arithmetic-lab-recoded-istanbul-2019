bdf1871642462f10757b2aefc4744cf8ee086947
Maher Suleyman <maher.suleyman.ms@gmail.com>
2020-06-15 06:34:45 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+let multiply,
+    random,
+    mod,
+    max;
+
+
+
+let num1 = 2    
+let num2 = 31    
+multiply = num1 * num2;
+random = Math.floor(Math.random() * 10) + 1;
+mod = 64 % 10
+max = Math.max(5, 11, 15, 2, 9, 1, 20)
